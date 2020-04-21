e2bcc680808eec64ec495f8cb99d2d883544d7e3
Tiago Silva <tiago10.ba@hotmmail.com>
2020-04-21 19:05:04 -0300

    Enumerações

---
+++ Enumeracoes/Program.cs
@@ -0,0 +1,32 @@
+﻿using Enumeracoes.Entities;
+using Enumeracoes.Entities.Enums;
+using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Enumeracoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            //Conversão para string
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus orderStatus = (OrderStatus)Enum.Parse(typeof(OrderStatus), "Delivered");
+
+            Console.WriteLine(txt);
+            Console.WriteLine(orderStatus);
+
+        }
+    }
+}
